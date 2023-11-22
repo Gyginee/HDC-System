130312e384d2df61696db943a99534b5865dc95e
Gyginee <hungnguyenminh66@gmail.com>
2023-11-22 16:07:46 +0700

    db update

---
+++ hdc-service/database/migrations/2023_11_20_093050_create_project_detail.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_cost', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->float('cost');
+            $table->float('internal_cost');
+            $table->float('real_cost');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_cost');
+    }
+};
